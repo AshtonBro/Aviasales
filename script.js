18e19f0ac3a385d93113c7a11132a7de8442f3d2
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-03-17 23:02:39 +0500

    take forms

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 'use script';
 
+const formSearch = document.querySelector('.form-search'),
+inputCitiesFrom = document.querySelector('.input__cities-from'),
+dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
+inputCitiesTo = document.querySelector('.input__cities-to'),
+dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
+inputDateDepart = document.querySelector('.input__date-depart');
+
+const city = ['Москва', 'Самара', 'Тюмень', 'Санкт-Петербург', 'Минск', 'Челябинск', 
+'Керч', 'Волгоград', 'Днепропетровск', 'Екатеринбург','Ульяновск', 'Нижневартовкс', 
+'Ростов-на-дону', 'Калининград', 'Нижний новгород', 'Одесса', 'Тобольск', 'Краснодар', 'Сочи'];
+
+inputCitiesFrom.addEventListener('input', () => {
+    
+});
