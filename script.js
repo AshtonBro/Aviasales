4c317d71b75fe69161b6ed2c5815ded018108d24
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-03-18 23:16:51 +0500

    db cities and getData

--- script.js
+++ script.js
@@ -7,9 +7,30 @@ inputCitiesTo = document.querySelector('.input__cities-to'),
 dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
 inputDateDepart = document.querySelector('.input__date-depart');
 
+const citiesApi = 'http://api.travelpayouts.com/data/ru/cities.json',
+    citiesDB = 'dataBase/cities.json',
+    proxy = '​https://cors-anywhere.herokuapp.com/';
+
 const city = ['Москва', 'Самара', 'Тюмень', 'Санкт-Петербург', 'Минск', 'Челябинск', 
 'Керч', 'Волгоград', 'Днепропетровск', 'Екатеринбург','Ульяновск', 'Нижневартовск', 
 'Ростов-на-дону', 'Калининград', 'Нижний новгород', 'Одесса', 'Тобольск', 'Краснодар', 'Сочи'];
+
+const getData = (url, callback) => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', url);
+
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState !== 4) {return;}
+
+        if (request.status === 200) {
+            callback(request.response);
+        } else {
+            console.error(request.status);
+        }
+    });
+    request.send();
+};
 
 const showCity = (input, list) => {
     list.textContent = '';
@@ -29,26 +50,31 @@ const showCity = (input, list) => {
     }
 };
 
-inputCitiesFrom.addEventListener('input', () => {
-    showCity(inputCitiesFrom, dropdownCitiesFrom);
-});
-
-dropdownCitiesFrom.addEventListener('click', (event) => {
+const selectCity = (event, input, list) => {
     const target = event.target;
     if (target.tagName.toLowerCase() === 'li') {
-        inputCitiesFrom.value = target.textContent;
-        dropdownCitiesFrom.textContent = '';
+        input.value = target.textContent;
+        list.textContent = '';
     }
+};
+
+
+inputCitiesFrom.addEventListener('input', () => {
+    showCity(inputCitiesFrom, dropdownCitiesFrom);
 });
 
 inputCitiesTo.addEventListener('input', () => {
     showCity(inputCitiesTo, dropdownCitiesTo);
 });
 
-dropdownCitiesTo.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.tagName.toLowerCase() === 'li') {
-        inputCitiesTo.value = target.textContent;
-        dropdownCitiesTo.textContent = '';
-    }
+dropdownCitiesFrom.addEventListener('input', () => {
+    showCity(event, inputCitiesFrom, dropdownCitiesFrom);
+});
+
+dropdownCitiesTo.addEventListener('input', () => {
+    showCity(event, inputCitiesTo, dropdownCitiesTo);
+});
+
+getData(citiesDB, (data) => {
+    console.log('data: ', data);
 });
