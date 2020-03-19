243a78322d4f1969d4f35c12b39160359d7a0c09
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-03-19 23:09:12 +0500

    render

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ inputCitiesTo = document.querySelector('.input__cities-to'),
 dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
 inputDateDepart = document.querySelector('.input__date-depart');
 
-const proxy = '​https://cors-anywhere.herokuapp.com/',
+const proxy = '​https://cors-anywhere.herokuapp.com',
     citiesAPI = 'http://api.travelpayouts.com/data/ru/cities.json',
     keyAPI = '0e0696b07d29106cb0c8007f0e7f46a9',
     calendar = 'http://min-prices.aviasales.ru/calendar_preload',
     citiesDB = 'dataBase/cities.json',
-     searchTickets = '?origin=SVX&destination=KGD&depart_date=2020-05-25&one_way=false';
+    searchTickets = '?origin=SVX&destination=KGD&depart_date=2020-05-25&one_way=false';
 
 let city = [];
 let tickets = [];
@@ -61,6 +61,14 @@ const selectCity = (event, input, list) => {
     }
 };
 
+const renderCheap = (data, date) => {
+    const cheapTicketYear = JSON.parse(data).best_prices;
+    console.log(cheapTicketYear);
+
+    const cheapTicketDay = cheapTicketYear.filter((item) => {
+        return item.depart_date === date;
+    });
+};
 
 inputCitiesFrom.addEventListener('input', () => {
     showCity(inputCitiesFrom, dropdownCitiesFrom);
@@ -78,15 +86,35 @@ dropdownCitiesTo.addEventListener('click', (event) => {
     selectCity(event, inputCitiesTo, dropdownCitiesTo);
 });
 
-/*
-getData(calendar, (data) => {
-    city = JSON.parse(data).filter((item) => item.name);
-}); */
+formSearch.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-getData(calendar + searchTickets, (data) => {
-    tickets = JSON.parse(data);
-    // Получаем массив минимальных цен, соответствующих заданной дате вылета
-    console.log(tickets.current_depart_date_prices);
-    //Получаем  массив минимальных цен для различных дней вылета
-    console.log( tickets.best_prices);
+    const cityFrom = city.find((item) => inputCitiesFrom.value === item.name);
+    const cityTo = city.find((item) => inputCitiesTo.value === item.name);
+
+    const fromData = {
+        from: cityFrom.code,
+        to: cityTo.code,
+        when: inputDateDepart.value,
+    };
+
+    const requestData = '?depart_date=' + fromData.when + '&origin=' + 
+    fromData.from + '&destination=' + fromData.to + '&one_way=true'; 
+
+    getData(calendar + requestData, (response) => {
+        console.log(response);
+    });
 });
+
+getData(citiesDB, (data) => {
+    city = JSON.parse(data).filter((item) => item.name);
+    console.log('city: ', city);
+});
+
+//  from: city.find((item) => inputCitiesFrom.value === item.name).code
+
+// getData(calendar + searchTickets + keyAPI, (data) => {
+//     tickets = JSON.parse(data).best_prices.filter(item => item.depart_date === '2020-05-29');
+//     // Получаем массив, соответствующих заданной дате вылета
+//     console.log(tickets);
+// });
